065fc5dea99c7d7396a981286b688df64003d41d
Daniel Gil Mendes <dangil.mendes@gmail.com>
2026-06-25 23:15:39 -0300

    chore: renomear ecossistema do PM2 para ecosystem.config.cjs por compatibilidade

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
         PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 3030,
         PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: './index.html'
+        PM2_SERVE_HOMEPAGE: '/index.html'
       }
     }
   ]
